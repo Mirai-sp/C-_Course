ceb55dafb2a702cbc37b3f90e4588fa806470124
Denis Queiroz <denisxx07@hotmail.com>
2022-07-17 14:26:56 -0300

    Adicao de conteudo

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -10,6 +10,7 @@ namespace Primeiro
             int idade = 32;
             double saldo = 10.35784;
             string nome = "Maria";
+            bool success = false;
 
             // Metodo Placeholder
             Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
@@ -22,6 +23,26 @@ namespace Primeiro
 
             // Para nao aplicar regionalizacao no ponto decimal e usar ponto ao invez de virgula
             Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+
+            // tomar cuidado com divisoes, se for numero inteiro a divisao sera inteira
+            double resultado = 10f / 2f;
+            Console.WriteLine(resultado);
+
+            // Fazer conversao de dado
+            while (!success)
+            {
+                try
+                {
+                    Console.WriteLine("Digite um número");
+                    int n1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Você digitou {0}", n1);
+                    success = true;
+                }
+                catch
+                {
+                    Console.WriteLine("O número digitado é inválido, tente novamente");
+                }
+            }
         }
     }
 }
